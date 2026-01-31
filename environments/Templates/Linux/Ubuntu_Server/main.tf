11d61f8383ec77588586fd332fcabe0c479f4ca6
Jens van den Hurk <jensvandenhurk@outlook.com>
2026-01-31 14:08:58 +0100

    Ubuntu server probleem opgelost

--- environments/Templates/Linux/Ubuntu_Server/main.tf
+++ environments/Templates/Linux/Ubuntu_Server/main.tf
@@ -2,7 +2,7 @@
 #Deze template is voor een Ubuntu server machine uit te rollen in de test/productie omgeving.
 
 resource "local_file" "ubuntu_server_vm" {
-  filename = ${var.ubuntu_server_vm}.txt
+  filename = "${var.ubuntu_server_vm}.txt"
 
   content = "This is an Ubuntu Server VM version ${var.ubuntu_server_version} with ${var.cpu} CPU cores and ${var.memory} MB RAM with ${var.disk_size} GB disk space. Created by Terraform in the local environment."
 
